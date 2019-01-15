582d524781010003d473c276a15a4b94f0b92d32
vaishnavi <vaish.jana@gmail.com>
2019-01-15 11:26:38 -0800

    Password reset confirm

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 #import timezone
 class Post(models.Model):
@@ -11,3 +11,6 @@ class Post(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
